34e95e0ea616828f39103a02fb519be8b09f180a
Dania Emmanuel <111269312+Godson-dan@users.noreply.github.com>
2022-09-14 00:12:47 +0100

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
-//print _putchar
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putvhar('h')
-	_putchar('a')
-	_putchar('r')
-	_putchar('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
