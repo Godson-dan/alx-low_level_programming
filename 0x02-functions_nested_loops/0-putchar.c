021726e51ef4e347356d58019ee9a322fc237335
Godson-dan <daniaemmanuel55@gmail.com>
2022-09-13 15:53:28 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+//print _putchar
+
+int main(void)
+{
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putvhar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')
+}
